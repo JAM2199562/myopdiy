6cc6d0642d359cf93941b2daeec23e09b909854a
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-11-23 22:46:29 +0800

    更改feeds设置

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -8,33 +8,25 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed '1 auci set luci.main.mediaurlbase=/luci-static/argon' -i package/lean/default-settings/files/zzz-default-settings
-sed '2 auci comit luci' -i package/lean/default-settings/files/zzz-default-settings
 
-# Add a feed source
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git luci_app_ddns_go https://github.com/sirpdboy/luci-app-ddns-go' feeds.conf.default
+# 增加feeds
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
+# 更新feeds
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-# ## small8的mosdns和v2ray-geodata有问题,liuran001的adg编译有问题
-
 # 安装v2ray-geodata
-find package -name v2ray-geodata | xargs rm -rf
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+find feeds/ -name "*geodata*" | xargs rm -rf
+find package/ -name "*geodata*" | xargs rm -rf
+git clone https://github.com/sbwml/v2ray-geodata package/geodata
 
-#安装mosdns
+# 安装mosdns
+find feeds/ -name "*mosdns*" | xargs rm -rf
+find package/ -name "*mosdns*" | xargs rm -rf
 git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+./scripts/feeds update -i
 
-#强制使用small8安装adguard
-./scripts/feeds install -f -p small8 adguardhome luci-app-adguardhome
-
-#安装passwall
-./scripts/feeds install -f -p small8 luci-app-passwall
-
-#安装passwall2
-./scripts/feeds install -f -p small8 luci-app-passwall2
-
-# 安装HelloWorld
-./scripts/feeds install -f -p jerryk luci-app-vssr
+# 安装ddns-go
+find feeds/ -name "*ddns-go*" | xargs rm -rf
+find package/ -name "*ddns-go*" | xargs rm -rf
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
