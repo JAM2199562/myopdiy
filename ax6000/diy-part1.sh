d327d5eb0be780b00af43a8f16920d8bc8b01dbb
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-11-25 01:19:42 +0800

    更改编译配置

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -9,24 +9,24 @@
 #=============================================================
 
 
-# 增加feeds
-# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# # 增加feeds
+# # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-# 更新feeds
-./scripts/feeds update -a && ./scripts/feeds install -a
+# # 更新feeds
+# ./scripts/feeds update -a && ./scripts/feeds install -a
 
-# 安装v2ray-geodata
-find feeds/ -name "*geodata*" | xargs rm -rf
-find package/ -name "*geodata*" | xargs rm -rf
-git clone https://github.com/sbwml/v2ray-geodata package/geodata
+# # 安装v2ray-geodata
+# find feeds/ -name "*geodata*" | xargs rm -rf
+# find package/ -name "*geodata*" | xargs rm -rf
+# git clone https://github.com/sbwml/v2ray-geodata package/geodata
 
-# 安装mosdns
-find feeds/ -name "*mosdns*" | xargs rm -rf
-find package/ -name "*mosdns*" | xargs rm -rf
-git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-./scripts/feeds update -i
+# # 安装mosdns
+# find feeds/ -name "*mosdns*" | xargs rm -rf
+# find package/ -name "*mosdns*" | xargs rm -rf
+# git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# ./scripts/feeds update -i
 
-# 安装ddns-go
-find feeds/ -name "*ddns-go*" | xargs rm -rf
-find package/ -name "*ddns-go*" | xargs rm -rf
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+# # 安装ddns-go
+# find feeds/ -name "*ddns-go*" | xargs rm -rf
+# find package/ -name "*ddns-go*" | xargs rm -rf
+# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
