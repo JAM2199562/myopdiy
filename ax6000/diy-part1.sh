809028cbbb2e87dd6e995d3174cc56f64b14c2c0
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-11-25 19:59:01 +0800

    更改软件包

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -10,23 +10,9 @@
 
 
 # # 增加feeds
-# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # # 更新feeds
-# ./scripts/feeds update -a && ./scripts/feeds install -a
+# # 这样设计的考虑是防止外挂软件包覆盖原作者的feeds设置
+./scripts/feeds update -a && ./scripts/feeds install -a
 
-# # 安装v2ray-geodata
-# find feeds/ -name "*geodata*" | xargs rm -rf
-# find package/ -name "*geodata*" | xargs rm -rf
-# git clone https://github.com/sbwml/v2ray-geodata package/geodata
-
-# # 安装mosdns
-# find feeds/ -name "*mosdns*" | xargs rm -rf
-# find package/ -name "*mosdns*" | xargs rm -rf
-# git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-# ./scripts/feeds update -i
-
-# # 安装ddns-go
-# find feeds/ -name "*ddns-go*" | xargs rm -rf
-# find package/ -name "*ddns-go*" | xargs rm -rf
-# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
