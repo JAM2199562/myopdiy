7713d1b7e9079d109e8f8cc4f9ee2259d8737d92
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-11-27 04:40:16 +0800

    使用x-wrt的dts文件

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -8,6 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+# # 使用x-wrt的dts文件
+wget -O target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000.dts https://tinyurl.com/x-wrt-ax6000-dts
 
 # # 增加feeds
 # # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
