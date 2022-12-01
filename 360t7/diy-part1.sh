9874b97054786de11333ba24a9379fe089a3a743
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-02 01:30:20 +0800

    删除base-files包

--- 360t7/diy-part1.sh
+++ 360t7/diy-part1.sh
@@ -12,6 +12,9 @@
 # # 增加feeds
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
+# # 删除kiddin9中会影响编译的包
+rm -rf feeds/kiddin9/base-files/
+
 # # 更新feeds
 # # 这样设计的考虑是防止外挂软件包覆盖原作者的feeds设置
 ./scripts/feeds update -a && ./scripts/feeds install -a
