dcab05a93742e81151fa1a9697ec62972a6e9537
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-02 17:05:29 +0800

    Update diy-part1.sh

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -12,8 +12,11 @@
 # # 增加feeds
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-# # 删除kiddin9中会影响编译的包
-rm -rf feeds/kiddin9/base-files/
 # # 更新feeds
 # # 这样设计的考虑是防止外挂软件包覆盖原作者的feeds设置
-./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a
+
+# # 删除kiddin9中会影响编译的包
+find feeds/ -name base-files | xargs rm -rf
+./scripts/feeds update -i
+./scripts/feeds install -a
