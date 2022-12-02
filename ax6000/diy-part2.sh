e6f1a061033187f03d1a1f2fab53a17f27e745c2
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-03 04:12:48 +0800

    文件名包含日期前缀

--- ax6000/diy-part2.sh
+++ ax6000/diy-part2.sh
@@ -7,12 +7,10 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-#sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%F-%H%M')' ./include/image.mk
-#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%F-%H%M')' ./include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 #sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='MyRouter'/g" ./package/base-files/files/bin/config_generate
 
-# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm
-# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
-# curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 sed -i 's/192.168.1.1/172.16.3.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/172.16/g' package/base-files/files/bin/config_generate
