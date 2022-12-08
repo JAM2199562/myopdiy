b1867ebea6669777debb075273c575b56bb878ad
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-09 00:23:08 +0800

    修改软件安装方法和固件名

--- 360t7/diy-part1.sh
+++ 360t7/diy-part1.sh
@@ -15,22 +15,17 @@ sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.co
 # # 更新feeds
 # # 这样设计的考虑是防止外挂软件包覆盖原作者的feeds设置
 ./scripts/feeds update -a
-rm -rf feeds/kiddin9/base-files/
+
+# # 删除kiddin9中会影响编译的包
+find feeds/ -name base-files | xargs rm -rf
+find feeds/ -name "*mosdns*" | xargs rm -rf
+find feeds/ -name "v2ray-geo*" | xargs rm -rf
+find package/ -name "*mosdns*" | xargs rm -rf
+find package/ -name "v2ray-geo*" | xargs rm -rf
+
+# 安装sbwml的luci-app-mosdns，这个版本的可以正常自动启动
+git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/geodata
+sed -i "s/mosdns.config.configfile/mosdns.mosdns.configfile/g" package/luci-app-mosdns/luci-app-mosdns/root/etc/init.d/mosdns
+./scripts/feeds update -i
 ./scripts/feeds install -a
-
-
-# # 安装v2ray-geodata
-# find feeds/ -name "*geodata*" | xargs rm -rf
-# find package/ -name "*geodata*" | xargs rm -rf
-# git clone https://github.com/sbwml/v2ray-geodata package/geodata
-
-# # 安装mosdns
-# find feeds/ -name "*mosdns*" | xargs rm -rf
-# find package/ -name "*mosdns*" | xargs rm -rf
-# git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-# ./scripts/feeds update -i
-
-# 安装ddns-go
-# find feeds/ -name "*ddns-go*" | xargs rm -rf
-# find package/ -name "*ddns-go*" | xargs rm -rf
-# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
