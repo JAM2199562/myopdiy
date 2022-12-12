ba65bd86aa9bf1d89ae629dc3021dcb3e510215f
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-13 00:51:32 +0800

    文件名包含编译时间

--- ax6000/diy-part2.sh
+++ ax6000/diy-part2.sh
@@ -7,8 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-#sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%F-%H%M')' ./include/image.mk
-#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%Y%m%d-%H%M')' ./include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 #sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='MyRouter'/g" ./package/base-files/files/bin/config_generate
 
