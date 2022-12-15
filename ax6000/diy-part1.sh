95000b899dbbdd511a45cd42a2426ed5104c5118
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-16 01:48:38 +0800

    add luci-app-mosdns

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -8,24 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-
 # # 增加feeds
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-# # 更新feeds
-# # 这样设计的考虑是防止外挂软件包覆盖原作者的feeds设置
-./scripts/feeds update -a
-
-# # # 删除kiddin9中会影响编译的包
-# find feeds/ -name base-files | xargs rm -rf
-# find feeds/ -name "*mosdns*" | xargs rm -rf
-# find feeds/ -name "v2ray-geo*" | xargs rm -rf
-# find package/ -name "*mosdns*" | xargs rm -rf
-# find package/ -name "v2ray-geo*" | xargs rm -rf
-
-# 安装sbwml的luci-app-mosdns，这个版本的可以正常自动启动
-# git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/geodata
-sed -i "s/mosdns.config.configfile/mosdns.mosdns.configfile/g" package/luci-app-mosdns/luci-app-mosdns/root/etc/init.d/mosdns
-./scripts/feeds update -i
-./scripts/feeds install -a
+#增加luci-app-mosdns
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
