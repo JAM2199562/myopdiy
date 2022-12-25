36f9573988b23fb0997ca62031d3e0907fc427ce
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-26 00:29:51 +0800

    修改主机名

--- ax6000/diy-part2.sh
+++ ax6000/diy-part2.sh
@@ -9,8 +9,7 @@
 #============================================================
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%Y%m%d-%H%M')' ./include/image.mk
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-#sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
-sed -i "s/hostname='OpenWrt'/hostname='MyRouter'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/hostname='ImmortalWrt'/hostname='MyRouter'/g" ./package/base-files/files/bin/config_generate
 
 sed -i 's/192.168.1.1/172.16.3.2/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/172.16/g' package/base-files/files/bin/config_generate
