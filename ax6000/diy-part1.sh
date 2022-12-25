2c96c811c2c4c4beb15648285427e57dc74f4397
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-26 00:36:30 +0800

    增加kenzok8软件源

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -8,11 +8,15 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# # 增加feeds
-# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-
 #增加luci-app-mosdns
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
 
 #增加luci-app-mosdns
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
+# # 增加feeds
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
