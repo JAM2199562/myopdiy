7b4d37de6dfead87d77af82a55d9c354689adda7
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-29 22:12:58 +0800

    更换软件源

--- immortalwrt/x86/diy-immortalwrt-part1.sh
+++ immortalwrt/x86/diy-immortalwrt-part1.sh
@@ -8,16 +8,16 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+#增加luci-app-mosdns
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
+sed -i 's/def_config.yaml/config.yaml/g' package/luci-app-mosdns/root/etc/config/mosdns
 
+#增加luci-app-ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 
-
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git luci_app_ddns_go https://github.com/sirpdboy/luci-app-ddns-go' feeds.conf.default
-
-# git clone https://github.com/IrineSistiana/mosdns package/mosdns
-git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/geodata
-./scripts/feeds update -a && ./scripts/feeds install -a
+# # 增加feeds
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
