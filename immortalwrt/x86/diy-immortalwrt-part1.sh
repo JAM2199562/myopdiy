3b07ff339379c32e90b29b0ff4fc6ea66925db26
Dikaiosyne <JAM2199562@users.noreply.github.com>
2022-12-31 04:12:01 +0800

    增加luci-app-lucky

--- immortalwrt/x86/diy-immortalwrt-part1.sh
+++ immortalwrt/x86/diy-immortalwrt-part1.sh
@@ -8,12 +8,15 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-#增加luci-app-mosdns
+# 增加luci-app-mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 sed -i 's/def_config.yaml/config.yaml/g' package/mosdns/luci-app-mosdns/root/etc/config/mosdns
 
-#增加luci-app-ddns-go
+# 增加luci-app-ddns-go
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
+# 增加luci-app-lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
