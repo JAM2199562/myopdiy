416eb7c48b6080e2e8f0e8f1dc542d138ed31596
Dikaiosyne <JAM2199562@users.noreply.github.com>
2023-01-13 21:11:25 +0800

    尝试修复alist的编译

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -22,8 +22,6 @@ git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 # cd $GITHUB_WORKSPACE/openwrt
 
 # 增加alist
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 ls -al feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 
