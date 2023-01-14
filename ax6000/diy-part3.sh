8c732a0ae2154912303d47ec32e9872db6bf79ac
Dikaiosyne <JAM2199562@users.noreply.github.com>
2023-01-15 04:44:12 +0800

    修复lucky不能运行

--- ax6000/diy-part3.sh
+++ ax6000/diy-part3.sh
@@ -3,3 +3,4 @@
 # 避免alist依赖的go在feeds操作时被覆盖掉
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+chmod a+x files/usr/bin/lucky
