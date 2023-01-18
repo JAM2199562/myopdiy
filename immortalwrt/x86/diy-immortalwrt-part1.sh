a74fcdb5aab63a69083eabbf46426d7eeafb4ea0
Dikaiosyne <JAM2199562@users.noreply.github.com>
2023-01-18 21:38:27 +0800

    自定义包增加z_前缀

--- immortalwrt/x86/diy-immortalwrt-part1.sh
+++ immortalwrt/x86/diy-immortalwrt-part1.sh
@@ -11,20 +11,20 @@
 # 增加luci-app-mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns package/z_mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/z_v2ray-geodata
 sed -i 's/def_config.yaml/config.yaml/g' package/mosdns/luci-app-mosdns/root/etc/config/mosdns
 
 # 增加luci-app-ddns-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/z_luci-app-ddns-go
 
 # 增加luci-app-lucky
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/z_lucky
 
 # 增加alist
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+git clone https://github.com/sbwml/luci-app-alist.git package/z_luci-app-alist
 
 # 增加adguard
-git clone https://github.com/kiddin9/openwrt-adguardhome.git package/openwrt-adguardhome
+git clone https://github.com/kiddin9/openwrt-adguardhome.git package/z_openwrt-adguardhome
