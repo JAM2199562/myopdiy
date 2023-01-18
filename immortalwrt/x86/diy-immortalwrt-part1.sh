8d226ca7b8e2185783ecd161705b6cdc49dc1b5e
Dikaiosyne <JAM2199562@users.noreply.github.com>
2023-01-18 23:14:16 +0800

    调整软件源

--- immortalwrt/x86/diy-immortalwrt-part1.sh
+++ immortalwrt/x86/diy-immortalwrt-part1.sh
@@ -3,16 +3,10 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
+# License: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-
-# 增加luci-app-mosdns
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns package/z_mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/z_v2ray-geodata
 
 # 增加luci-app-mosdns
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/z_luci-app-mosdns
@@ -21,12 +15,14 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/z_luci-app-ddns-go
 
 # 增加luci-app-lucky
-git clone  https://github.com/gdy666/luci-app-lucky.git package/z_lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/z_lucky
 
 # 增加alist
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+ls -al feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist.git package/z_luci-app-alist
 
 # 增加adguard
 git clone https://github.com/kiddin9/openwrt-adguardhome.git package/z_openwrt-adguardhome
+
+# 增加v2ray-geodata
+# git clone https://github.com/sbwml/v2ray-geodata package/z_v2ray-geodata
