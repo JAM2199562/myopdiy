14a2d0fb25047b0dfb890a234f3749f3cf283b1a
Dikaiosyne <JAM2199562@users.noreply.github.com>
2023-10-05 16:36:58 +0800

    不自定义go路径

--- ax6000/diy-part1.sh
+++ ax6000/diy-part1.sh
@@ -8,12 +8,14 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+# upx相关的设置
+# git clone https://github.com/kuoruan/openwrt-upx.git package/mycustom/openwrt-upx &&\
+# cd package/mycustom/openwrt-upx &&\
+# git checkout -b e5b33e219922f427a04372f5eb7b1bb0cc8db848
+# cd ../../..
+
 # 增加luci-app-mosdns
 rm -rf ./feeds/packages/net/mosdns
-git clone https://github.com/kuoruan/openwrt-upx.git package/mycustom/openwrt-upx &&\
-cd package/mycustom/openwrt-upx &&\
-git checkout -b e5b33e219922f427a04372f5eb7b1bb0cc8db848
-cd ../../..
 git clone  https://github.com/QiuSimons/openwrt-mos package/openwrt-mos \
 && cd package/openwrt-mos && git checkout 40b669b564e3876cd02aeb148fccd576647394cc \
 && mv -n {*mosdns,v2ray-geodata} ../ && cd ../.. && rm -rf package/openwrt-mos
